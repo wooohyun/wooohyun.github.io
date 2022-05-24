8245515f6137368904c72cc1af15673173952928
wooohyun <105770969+wooohyun@users.noreply.github.com>
2022-05-24 18:16:01 +0900

    Add files via upload

--- trn01/js/main.js
+++ trn01/js/main.js
@@ -1,9 +1,10 @@
 $(function () {
-    $('.first_slider').slick({
+    $('.main_slider').slick({
         arrows: false,
         autoplay: true,
-        dots: true,
-    });
+        pauseOnHover: false,
+        pauseOnFocus: false,
+    })
+});
 
-})
 // $(function(){}) 상단에 jquery적을려면 사용 하단에 적어도 보통적어줌
