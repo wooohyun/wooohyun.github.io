e3277c75bc4720c48dde3ae43e2c3a9a74af95e9
wooohyun <105770969+wooohyun@users.noreply.github.com>
2022-05-27 18:20:08 +0900

    Add files via upload

--- trn01/js/main.js
+++ trn01/js/main.js
@@ -1,5 +1,11 @@
 $(function () {
 
+    $('.main_slider').on('init reInit afterChange', function () {
+        var here = $('.slick-current');
+        here.addClass('on').siblings().removeClass('on')
+    })
+
+
     $('.main_slider').slick({
         arrows: false,
         autoplay: true,
@@ -7,14 +13,6 @@ $(function () {
         pauseOnFocus: false,
         fade: true,
     })
-
-
-    $('.main_slider').on('init reInit afterchange', function () {
-        var here = $('.slick-current');
-        here.addclass('on').siblings().removeclass('on')
-    })
-
-
 })
 
 // $(function(){}) 상단에 jquery적을려면 사용 하단에 적어도 보통적어줌
