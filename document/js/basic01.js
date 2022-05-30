c7e63939548233e3b73422242cddc923c82e7b39
wooohyun <105770969+wooohyun@users.noreply.github.com>
2022-05-30 17:41:54 +0900

    Add files via upload

---
+++ document/js/basic01.js
@@ -0,0 +1,6 @@
+// 2022.05.30 jwh
+// document.querySelector('header').style.color = 'red'
+document.querySelector('.mopen').addEventListener('click', function () {
+    document.querySelector('nav').classList.toggle('on');
+    this.classList.toggle('on');
+})
